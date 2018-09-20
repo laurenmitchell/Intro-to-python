26f3e8bc2211a8ea4805c38a245bedc0863c0b3e
laurenmitchell <lauren.a.mitchell@colorado.edu>
2018-09-20 09:33:58 -0400

    Lists-dictionaries-tuple notes and class work

---
+++ lists:dicts:tuples.py
@@ -0,0 +1,14 @@
+#lists/dictionaries/tuples
+
+#create a list and show how we can find certain items in list
+#add item to list
+
+# tuples cannot be changed once created!
+# helpful because sometimes you want to create
+# something that you know will never change
+fibs = (0,1,1,2,3)
+
+#dictionaries aka dicts aka map
+#key:value pairing
+
+favoriteSports = {}
