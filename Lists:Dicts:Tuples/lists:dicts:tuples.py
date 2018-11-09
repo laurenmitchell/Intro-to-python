d41ffef414154cfcb00df22958f02d6dd96cc4af
laurenmitchell <lauren.a.mitchell@colorado.edu>
2018-11-09 09:08:10 -0500

    lists and dicationaries

--- Lists:Dicts:Tuples/lists:dicts:tuples.py
+++ Lists:Dicts:Tuples/lists:dicts:tuples.py
@@ -2,11 +2,13 @@
 
 #create a list and show how we can find certain items in list
 #add item to list
-
+list1 = []
+print("this is an example of a list: ", list1)
 # tuples cannot be changed once created!
 # helpful because sometimes you want to create
 # something that you know will never change
 fibs = (0,1,1,2,3)
+print("this is an example of a tuple: ", fibs)
 
 #dictionaries aka dicts aka map
 #key:value pairing
