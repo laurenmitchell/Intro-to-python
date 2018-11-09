bfb00a3751fe300d4fc0d6d933428b0d90997e5e
laurenmitchell <lauren.a.mitchell@colorado.edu>
2018-11-09 09:10:17 -0500

    Taylor Swift lyrics and python teplate

---
+++ tswift_read_txt/tswift_read_txt.py
@@ -0,0 +1,9 @@
+#read in taylor swift lyrics
+# Due 11/13: Reading a .txt file
+# Let’s see how often Taylor Swift says the word “love” in her songs.
+# First, read in the .txt file provided to you on Github.
+# Create a for loop to write count how many times the word “love” occurs
+# need to split lines of words
+# Read each line from the file and split the line to form a list of words.
+# Check if the word provided by the user and any of the words in the list are equal and if they are, increment the word count.
+# So, you will need to create “nested” for loop (two for loops)
